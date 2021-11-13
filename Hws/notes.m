0ce2ab2b2b2b9b0faf3daaa159c9f40732e21ccc
Liza Kaniewski <lkaniewski@wustl.edu>
2021-11-13 13:42:43 -0600

    notes doc

---
+++ Hws/notes.m
@@ -0,0 +1,6 @@
+%% Nov 13 liza notes
+%things I think need to change: 
+%1) Initial conditions of the steady state systems
+%2) the first gain I think should be the negative of the first pole?? 
+%3) the commands are then going to be step inputs
+%%
