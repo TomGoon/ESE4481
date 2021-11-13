07b0fd8e9cdc44d884b74cdd242b2febce95a9fc
Liza Kaniewski <lkaniewski@wustl.edu>
2021-11-13 13:37:20 -0600

    controls

---
+++ Hws/ESE4481_hw4_poleplacement.m
@@ -0,0 +1,11 @@
+K_T = place(A_throttle, B_throttle, [-2,-4])
+K_A = place(A_aileron, B_aileron, [-2,-4,-1]);
+K_E = place(A_elevator, B_elevator, [-2,-4,-1]);
+K_R = place(A_rudder, B_rudder, [-2,-4]);
+
+
+
+
+
+
+
