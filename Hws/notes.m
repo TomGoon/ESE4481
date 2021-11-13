dcd046479d083139d4dbd667a475897aeb950aef
Liza Kaniewski <lkaniewski@wustl.edu>
2021-11-13 14:08:01 -0600

    notes doc

--- Hws/notes.m
+++ Hws/notes.m
@@ -2,5 +2,6 @@
 %things I think need to change: 
 %1) Initial conditions of the steady state systems
 %2) the first gain I think should be the negative of the first pole?? 
+    %no it should be feed forwaard gain? 
 %3) the commands are then going to be step inputs
 %%
